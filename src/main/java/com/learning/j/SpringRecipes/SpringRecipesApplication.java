e04f20c3817d65ee71a530205441f46ac17b01ff
TheGr3atJ <jmchikhaliya007@gmail.com>
2021-06-10 00:23:14 +0530

    1-1 : First Commit

---
+++ src/main/java/com/learning/j/SpringRecipes/SpringRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.learning.j.SpringRecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/j/SpringRecipes/SpringRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.j.SpringRecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
